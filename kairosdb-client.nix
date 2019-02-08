8578187bef02d1a2612b52094fec3a42bd53d1c6
Thomas Bach <hacking@babibo.de>
2019-02-08 10:50:20 +0100

    added basic dependencies

--- kairosdb-client.nix
+++ kairosdb-client.nix
@@ -1,12 +1,17 @@
-{ mkDerivation, base, hspec, hspec-discover, stdenv }:
+{ mkDerivation, aeson, base, bytestring, containers, hspec
+, hspec-discover, scientific, stdenv, text, time
+}:
 mkDerivation {
   pname = "kairosdb-client";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base ];
-  testHaskellDepends = [ base hspec hspec-discover ];
+  libraryHaskellDepends = [
+    aeson base bytestring containers scientific text time
+  ];
+  testHaskellDepends = [
+    aeson base bytestring containers hspec hspec-discover time
+  ];
   testToolDepends = [ hspec-discover ];
-  doHaddock = false;
   description = "KairosDB client for Haskell";
   license = stdenv.lib.licenses.bsd3;
 }
