d48a2044f621985563088a233ffcb6d7c33ee3ae
Thomas Bach <hacking@babibo.de>
2019-02-08 16:41:44 +0100

    provide documentation

--- src/Database/KairosDB/Internal/Types.hs
+++ src/Database/KairosDB/Internal/Types.hs
@@ -1,12 +1,17 @@
 -- |
 -- Copyright:  2019 Thomas Bach
 -- License  :  BSD3
---
 -- Maintainer: Thomas Bach <hacking@babibo.de>
+--
+-- `data` and `newtype` declarations to represent communication with
+-- KairosDB. This basically boils down to find a good representation
+-- of the JSON instances. We try to be as close to KairosDB's
+-- nomenclature as possible.
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Database.KairosDB.Internal.Types
-    ( QueryResponse(..)
+    ( -- * Query Responses from KairosDB
+      QueryResponse(..)
     , DataPointGroup(..)
     , GroupBy(..)
     , KairosTimestamp(..)
@@ -24,22 +29,28 @@ import Data.Time        (UTCTime (UTCTime), addUTCTime, fromGregorian,
                          secondsToDiffTime)
 import GHC.Generics     (Generic)
 
+-- | Responses from KairosDB are wrapped in an additional `{
+-- "queries": [ ... results ... ] }`. You should not observe this type
+-- in any high-level functions.
 newtype WrappedQueryResponse =
     WrappedQueryResponse { queries :: [QueryResponse] }
     deriving (Eq, Generic, Show)
 
 instance FromJSON WrappedQueryResponse
 
-data QueryResponse = QueryResponse { sampleSize :: Integer
-                                   , results    :: [DataPointGroup]
-                                   }
-                   deriving (Eq, Show)
+data QueryResponse = QueryResponse
+    { sampleSize :: Int              -- ^ The size of the result
+    , results    :: [DataPointGroup]
+    }
+    deriving (Eq, Show)
 
 instance FromJSON QueryResponse where
     parseJSON = withObject "QueryResponse" $ \v -> QueryResponse
                     <$> v .: "sample_size"
                     <*> v .: "results"
 
+-- | A group of data points where a data point is a tuple of timestamp
+-- and value.
 data DataPointGroup = DataPointGroup
     { name    :: Text
     , tags    :: Map Text [Text]
@@ -55,17 +66,21 @@ instance FromJSON DataPointGroup where
                     <*> (fromMaybe mempty <$> (v .:? "group_by"))
                     <*> v .: "values"
 
+-- | KairosDB represents time as milliseconds since epoch. This is a
+-- simple wrapper around 'UTCTime' to ensure proper JSON
+-- (de-)serialization.
 newtype KairosTimestamp = KairosTimestamp { getUTCTime :: UTCTime }
                         deriving (Eq, Show)
 
 instance FromJSON KairosTimestamp where
     parseJSON v@(Number _) =
-        KairosTimestamp . (`addUTCTime` utc0) . fromMilliSecs <$> parseJSON v
+        KairosTimestamp . (`addUTCTime` epoch) . fromMilliSecs <$> parseJSON v
       where
-        utc0 = UTCTime (fromGregorian 1970 1 1) (secondsToDiffTime 0)
+        epoch = UTCTime (fromGregorian 1970 1 1) (secondsToDiffTime 0)
         fromMilliSecs x = x / realToFrac 1000
     parseJSON v = typeMismatch "Milliseconds" v
 
+-- | Haskell representation of the several ways to group data points.
 data GroupBy = GroupByType Text
              deriving (Eq, Show)
 
