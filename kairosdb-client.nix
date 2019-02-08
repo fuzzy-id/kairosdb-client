28b5e2b9ecfdf24ea84d5dd3277de973662ebdd6
Thomas Bach <hacking@babibo.de>
2019-02-08 10:30:14 +0100

    added test machinery via hspec-discover

--- kairosdb-client.nix
+++ kairosdb-client.nix
@@ -1,9 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, hspec, hspec-discover, stdenv }:
 mkDerivation {
   pname = "kairosdb-client";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ];
+  testHaskellDepends = [ base hspec hspec-discover ];
+  testToolDepends = [ hspec-discover ];
   doHaddock = false;
   description = "KairosDB client for Haskell";
   license = stdenv.lib.licenses.bsd3;
