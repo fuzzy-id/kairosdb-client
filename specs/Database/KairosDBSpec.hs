e86b63c782e07fa5c87402d21a78eed5abdc07c0
Thomas Bach <hacking@babibo.de>
2019-02-22 13:32:13 +0100

    finally a test for adding and querying values

--- specs/Database/KairosDBSpec.hs
+++ specs/Database/KairosDBSpec.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Database.KairosDBSpec (spec) where
 
-import Data.HashMap.Strict (fromList)
-import Test.Hspec          (Spec, beforeAll, describe, it, shouldReturn)
+import Control.Concurrent  (threadDelay)
+import Data.HashMap.Strict (fromList, singleton)
+import Data.Time           (getCurrentTime)
+import Test.Hspec          (Spec, beforeAll, describe, it, runIO, shouldBe,
+                            shouldReturn, shouldSatisfy)
 
 import Database.KairosDB
 
@@ -24,6 +27,36 @@ spec = beforeAll kairosDBSettingsFromEnv $ do
             datapoints settings ([] :: [DataPoints ()]) `shouldReturn` ()
         it "should work on empty datapoints" $ \settings ->
             datapoints settings [emptyDataPoints] `shouldReturn` ()
+    describe "adding and querying points" $
+        do now <- KairosTimestamp <$> runIO getCurrentTime
+           let q = QueryMetrics (Right nowMinusOneSec) Nothing Nothing [Metric "some-name" mempty Nothing mempty]
+               nowMinusOneSec = addToKairosTimestamp (toEnum $ -10^(12 :: Int)) now
+           it "should return empty response first" $ \settings ->
+               query settings q
+                   `shouldReturn`
+                       ([QueryResponse 0 [DataPointGroup "some-name" mempty mempty mempty]] :: [QueryResponse Int])
+           it "should add actual values" $ \settings ->
+               datapoints settings [emptyDataPoints {dataPointsPoints = [(now, 1 :: Int)]}]
+                   `shouldReturn` ()
+           it "should then be able to query these values" $ \settings -> do
+               threadDelay 500000
+               resp <- query settings q
+               resp `shouldSatisfy` ((== 1) . length)
+               let [QueryResponse num dpGroup] = resp
+               num `shouldBe` 1
+               dpGroup `shouldSatisfy` ((== 1) . length)
+               let [DataPointGroup name tags groupBy vals] = dpGroup
+               name `shouldBe` "some-name"
+               tags `shouldBe` singleton "foo" ["bar"]
+               groupBy `shouldBe` [GroupByType "number"]
+               vals `shouldSatisfy` ((== 1) . length)
+               let [(ts, val)] = vals
+               val `shouldBe` (1 :: Int)
+               ts `shouldSatisfy` (> nowMinusOneSec)
+               ts `shouldSatisfy` (< now)
+               -- [QueryResponse 1 [DataPointGroup "some-name" (singleton "foo" ["bar"]) [GroupByType "number"] [(now, 1 :: Int)]]]
+
+
 
 emptyReq :: QueryMetrics
 emptyReq = QueryMetrics (Left (Relative 1 Years)) Nothing Nothing []
